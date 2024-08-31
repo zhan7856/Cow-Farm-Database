68b928cc6346691eb884785b10c85dfb37c1b63d
Paulina <zhan7856>
2024-08-31 11:38:55 -0500

    update front-end

---
+++ farm-management/src/main/java/com/paulina/farm_management/FarmManagementApplication.java
@@ -0,0 +1,13 @@
+package com.paulina.farm_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FarmManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FarmManagementApplication.class, args);
+	}
+
+}
