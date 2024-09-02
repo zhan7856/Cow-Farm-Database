d788ad02ac5bc2b892ac84ad0e8a896025e919ba
Paulina <zhan7856>
2024-09-02 17:58:33 -0500

    edit repository

--- farm-management/src/main/java/com/paulina/farm_management/repository/TransportCompanyRepository.java
+++ farm-management/src/main/java/com/paulina/farm_management/repository/TransportCompanyRepository.java
@@ -4,5 +4,5 @@ import com.paulina.farm_management.data_model.TransportCompany;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface TransportCompanyRepository extends JpaRepository<TransportCompanyRepository, Integer> {
+public interface TransportCompanyRepository extends JpaRepository<TransportCompany, Integer> {
 }
--- farm-management/src/main/java/com/paulina/farm_management/repository/TransportRepository.java
+++ farm-management/src/main/java/com/paulina/farm_management/repository/TransportRepository.java
@@ -1,8 +1,8 @@
 package com.paulina.farm_management.repository;
 
-import com.paulina.farm_management.data_model.Tank;
+import com.paulina.farm_management.data_model.Transport;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface TransportRepository extends JpaRepository<Tank, Integer> {
+public interface TransportRepository extends JpaRepository<Transport, Integer> {
 }
